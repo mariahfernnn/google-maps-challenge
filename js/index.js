df811aaee2f6b1c8b4cade87f0f58af9516e5f38
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-05-27 16:21:22 -0400

    Creating a new function - to loop through the different spots - follow-along

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 var map;
-  function initMap() {
-    var torontoCanada = {
-      lat: 43.651070,
-      lng: -79.347015
-    }
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: torontoCanada,
-      zoom: 11
-    });
+function initMap() {
+  var torontoCanada = {
+    lat: 43.651070,
+    lng: -79.347015
   }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: torontoCanada,
+    zoom: 11
+  });
+}
+
+function displaySpots() {
+  
+}
+
