f2b4ae57eb50fafcf3f1ea6cf570df38b0bd0805
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-06-02 18:24:04 -0400

    Fixing a bug - phone # is also linkable to Google Maps

--- js/index.js
+++ js/index.js
@@ -305,7 +305,6 @@ function createMarker(latlng, name, address, statusText, phone, index) {
                   <i class="fas fa-location-arrow"></i>
               </div>
               <a href="https://www.google.com/maps/dir/?api=1&destination=${address}">
-              ${address}
           </div>
           <div class="store-info-phone">
               <div class="circle">
