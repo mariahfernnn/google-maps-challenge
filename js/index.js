0c6571d0655fa87b509aab5b18ac57e0250809ec
mariahfernnn <maria.h.fernandez@ryerson.ca>
2020-06-02 18:49:07 -0400

    Still trying to fix issue - now address is not rendering either

--- js/index.js
+++ js/index.js
@@ -304,7 +304,6 @@ function createMarker(latlng, name, address, statusText, phone, index) {
               <div class="circle">
                   <i class="fas fa-location-arrow"></i>
               </div>
-              <a href="https://www.google.com/maps/dir/?api=1&destination=${address}">
               ${address}
           </div>
           <div class="store-info-phone">
